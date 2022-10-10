e65db9fc1a28e8308a1a3ed820b9cf533a2b519c
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2022-10-10 10:07:45 +0200

    modified types

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -19,29 +19,56 @@ export interface CartProductProps {
   quantity: number;
 }
 
-export interface OrderFormProps {
-  shipping: {
-    first_name: string;
-    last_name: string;
-    address: string;
-    city: string;
-    state: string;
-    postal_code: string;
-    email: string;
-    phone_number: string;
-  };
-  billing: {
-    name_on_card: string;
-    address: string;
-    city: string;
-    state: string;
-    postal_code: string;
-    email: string;
-    phone_number: string;
-    card_number: string;
-    card_expiration: string;
-    cvv: string;
-  };
+export interface BillingFormProps {
+  name_on_card: string;
+  address: string;
+  city: string;
+  state: string;
+  postal_code: string;
+  email: string;
+  phone_number: string;
+  card_number: string;
+  card_expiration: string;
+  cvv: string;
+}
+
+export interface ShippingFormProps {
+  first_name: string;
+  last_name: string;
+  address: string;
+  city: string;
+  state: string;
+  postal_code: string;
+  email: string;
+  phone_number: string;
+}
+
+export interface CostProps {
+  subtotal: number;
+  tax: number;
+  shipping: number;
+  total: number;
+}
+
+export interface OrderProductsProps {
+  id: number;
+  name: string;
+  quantity: number;
+}
+
+export interface IsValidProps {
+  shipping: boolean;
+  billing: boolean;
+  confirmed: boolean;
+}
+
+export interface FinalOrderProps {
+  order_number: string;
+  order_products: OrderProductsProps[];
+  store_pick_up: boolean;
+  shipping: ShippingFormProps;
+  billing: BillingFormProps;
+  cost: CostProps;
 }
 
 export type SvgComponentAsProps = React.FunctionComponent<
